c3be8a6ff16da8848bfac1de67bf87ab656116b5
caio bonetto prazeres <caioprazeres1@gmail.com>
2024-10-08 18:51:22 -0300

    icones

--- src/app/modules/mainpage/pages/home/home.component.ts
+++ src/app/modules/mainpage/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
