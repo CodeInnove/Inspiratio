1b7ef7829f3caf9da88d52f0a89ac2958bad8902
leoocestari <leonardocestari67@gmail.com>
2024-10-08 21:55:41 -0300

    calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    
+    CommonModule,
+    
+  ],
   template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {}
+export class AppComponent {
+  date = new Date();
+
+  onDateChange(event: MatDatepickerInputEvent<Date>) {
+    console.log(event.value);
+  }
+}
--- src/app/modules/mainpage/pages/home/home.component.ts
+++ src/app/modules/mainpage/pages/home/home.component.ts
@@ -3,12 +3,29 @@ import { MatIcon } from '@angular/material/icon';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms'; // Importe o módulo aqui
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatIcon, MatDatepickerModule, MatNativeDateModule, MatInputModule ],
+  imports: [
+    MatIcon, 
+    MatDatepickerModule, 
+    MatNativeDateModule, 
+    MatInputModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {}
+export class HomeComponent {
+  selectedDate: Date | null = null;
+
+  onDateChange(event: Date){
+    console.log('Data Selecionada', event);
+    this.selectedDate = event;
+  }
+}
