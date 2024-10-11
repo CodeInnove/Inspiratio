a9a056e52f4787c6c16a33fc00f6c220f8a8f216
leoocestari <leonardocestari67@gmail.com>
2024-10-10 22:22:16 -0300

    homep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    
-    CommonModule,
-    
   ],
   template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {
-  date = new Date();
 
-  onDateChange(event: MatDatepickerInputEvent<Date>) {
-    console.log(event.value);
   }
-}
--- src/app/modules/mainpage/pages/home/home.component.ts
+++ src/app/modules/mainpage/pages/home/home.component.ts
@@ -1,31 +1,19 @@
 import { Component } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms'; // Importe o módulo aqui
-import { MatDatepickerInputEvent } from '@angular/material/datepicker';
-import { CommonModule } from '@angular/common';
+import {ChangeDetectionStrategy, model} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    MatIcon, 
-    MatDatepickerModule, 
-    MatNativeDateModule, 
-    MatInputModule,
-    ReactiveFormsModule,
-    CommonModule,
-  ],
+  imports: [MatCardModule, MatDatepickerModule],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
-  selectedDate: Date | null = null;
+  selected = model<Date | null>(null);
 
-  onDateChange(event: Date){
-    console.log('Data Selecionada', event);
-    this.selectedDate = event;
-  }
 }
