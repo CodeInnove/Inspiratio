0a179bc45a2d9be555bfb87b1e2b1e5b3ea9befe
caio bonetto prazeres <caioprazeres1@gmail.com>
2024-10-13 21:59:13 -0300

    side navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/mainpage/pages/home/home.component';
 import { LoginComponent } from './modules/mainpage/pages/login/login.component';
+import { NavbarComponent } from './modules/mainpage/components/navbar/navbar.component';
 
 
 export const routes: Routes = [
@@ -12,6 +13,10 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'nav',
+    component: NavbarComponent
   }
   
 ];
--- src/app/modules/mainpage/components/navbar/navbar.component.ts
+++ src/app/modules/mainpage/components/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  title = 'nav';
 }
